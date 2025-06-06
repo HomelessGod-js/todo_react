c44c6056af7959caeedde4492e8d593035cc90ed
Dmitry Povetkin <1_miko_1@mail.ru>
2025-06-06 15:35:30 +0300

    todo_react_first

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+const root = createRoot(document.getElementById('todoapp'))
+
+root.render(<App />)
